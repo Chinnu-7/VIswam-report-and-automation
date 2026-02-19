6bf8e2afca992f3d5868ada062a6a8390eaf47fb
Deployment Helper <deploy-bot@example.com>
2026-02-19 12:38:58 +0530

    Restore basic dependencies to api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,8 +1,17 @@
 import serverless from 'serverless-http';
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
 
 const app = express();
-app.get('/.netlify/functions/api/ping', (req, res) => res.json({ message: 'pong', time: new Date().toISOString() }));
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/ping', (req, res) => res.json({ message: 'pong', time: new Date().toISOString() }));
 
 export const handler = serverless(app);
+
 
